7824e8c697250cce281e413e3624ba1b0d7d99fa
Sskr2000us <sskr2000us@gmail.com>
2025-12-29 17:22:04 -0600

    Switch to production backend and fix CardTheme type

--- apps/mobile/lib/services/api_client.dart
+++ apps/mobile/lib/services/api_client.dart
@@ -13,9 +13,6 @@ class ApiClient {
     
     // Local development - uncomment to use localhost
     // if (kIsWeb) return 'http://localhost:8000';
-    // if (defaultTargetPlatform == TargetPlatform.android) {
-    //   return 'http://10.0.2.2:8000';
-    // }
     // return 'http://localhost:8000';
   }
 
